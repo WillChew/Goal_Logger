2c533f6fa2753d81d5c14cbd9c114cf9681c5b87
WillChew <williamchew95@gmail.com>
2019-11-08 23:12:41 -0500

    Initial Commit

---
+++ Goal_Logger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Goal_Logger
+//
+//  Created by Will Chew on 2019-11-08.
+//  Copyright © 2019 Will Chew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
